9b5e89d1a97b9f8b244dff005165dc01bdf0ee9b
Nicolas Petras <nicolaspetras@gmail.com>
2021-07-24 16:11:17 +0100

    SFLA-17 feat: set-up of the Spring back-end

---
+++ backend/smite-fantasy/src/main/kotlin/com/nicolaspetras/smitefantasy/SmiteFantasyApplication.kt
@@ -0,0 +1,11 @@
+package com.nicolaspetras.smitefantasy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SmiteFantasyApplication
+
+fun main(args: Array<String>) {
+    runApplication<SmiteFantasyApplication>(*args)
+}
