9e3f39ce96522b935f02b37c3436446fab0e1f38
Nicolas Petras <nicolaspetras@gmail.com>
2024-03-21 22:47:13 +0000

    refactor: project for separate scraper service

---
+++ backends/spl-fantasy-web-scraper/src/test/kotlin/com/nicolaspetras/splfantasywebscraper/SplFantasyWebScraperApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nicolaspetras.splfantasywebscraper
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SplFantasyWebScraperApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
