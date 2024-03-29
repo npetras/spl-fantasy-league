3164d7a4c046a6657d05669bd500d2f0b0e10982
Nicolas Petras <nicolaspetras@gmail.com>
2024-03-29 23:56:45 +0000

    refactor(Score): created Score backend component project
    Updated name from Scorer to Score - using nouns for all components

---
+++ backends/score/src/test/kotlin/com/nicolaspetras/splfantasy/score/ScoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nicolaspetras.splfantasy.score
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ScoreApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
