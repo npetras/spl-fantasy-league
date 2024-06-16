15da267c08cbc00b1c77eeb0e982da0cde5e4f1b
Nicolas Petras <nicolaspetras@gmail.com>
2024-06-16 22:16:09 +0100

    refactor(playerStats): update defaults to 0 from -1

--- backend/src/main/kotlin/com/nicolaspetras/splfantasy/model/stat/collection/SplPlayerStats.kt
+++ backend/src/main/kotlin/com/nicolaspetras/splfantasy/model/stat/collection/SplPlayerStats.kt
@@ -10,15 +10,15 @@ data class SplPlayerStats(
     val name: String = "",
     val splTeam: SplTeamName = SplTeamName.NONE,
     val role: SmiteRole = SmiteRole.NONE,
-    val kills: Int = -1,
-    val deaths: Int = -1,
-    val assists: Int = -1,
-    var mitigatedDamage: Int = -1,
-    var playerDamage: Int = -1,
-    var goldPerMin: Int = -1,
-    var wards: Int = -1,
-    var structureDamage: Int = -1,
-    var healing: Int = -1
+    val kills: Int = 0,
+    val deaths: Int = 0,
+    val assists: Int = 0,
+    var mitigatedDamage: Int = 0,
+    var playerDamage: Int = 0,
+    var goldPerMin: Int = 0,
+    var wards: Int = 0,
+    var structureDamage: Int = 0,
+    var healing: Int = 0
 ) {
 
 }
